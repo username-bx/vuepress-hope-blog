aea7937a51018617aefe4d12384fc5ed20072351
bx <bx@gmail.com>
2023-11-03 11:24:19 +0800

    调整导航栏 调整侧边栏 调整文章顺序

---
+++ assets/index.html-09868944.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c,d as e}from"./app-ed384d29.js";const n={};function r(s,l){return t(),c("div",null,[e(" 这是一个博客主页的案例。 "),e(" 要使用此布局，你应该在页面前端设置 `layout: BlogHome` 和 `home: true`。 "),e(" 相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home/)。 ")])}const _=o(n,[["render",r],["__file","index.html.vue"]]);export{_ as default};
