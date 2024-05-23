c3c0ff64ce7067dabb4c6beafa53303bd30b36c2
Benjamin Ma <benjamin_ma@manka.co.jp>
2024-05-23 09:30:46 +0900

    20240523 添加文章

---
+++ assets/pageview-86f6213f.js
@@ -0,0 +1,1 @@
+import{l as t}from"./app-65608fb9.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-65608fb9.js").then(r=>r.Q),[]);return e({serverURL:"https://comments.luogesidoc.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
---
+++ assets/ssh2.html-d67ce068.js
@@ -0,0 +1,19 @@
+import{_ as s,o as e,c as i,e as a}from"./app-65608fb9.js";const n={},l=a(`<h1 id="使用ssh同时连接gitlab-github" tabindex="-1"><a class="header-anchor" href="#使用ssh同时连接gitlab-github" aria-hidden="true">#</a> 使用ssh同时连接gitlab github</h1><ol><li><p>打开文件夹 c:/user/xxx/.ssh</p></li><li><p>右键 git bash here</p></li><li><p>生成ssh</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ ssh-keygen <span class="token parameter variable">-t</span> ed25519 <span class="token parameter variable">-C</span> <span class="token string">&quot;bx.ma@gmail.com&quot;</span>
+// 输入名称 id_ssh_gitlab
+$ ssh-keygen <span class="token parameter variable">-t</span> ed25519 <span class="token parameter variable">-C</span> <span class="token string">&quot;bx.ma@gmail.com&quot;</span>
+// 输入名称 id_ssh_github
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div></li><li><p>启动ssh-agent</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ ssh-agent <span class="token function">bash</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div></li><li><p>添加ssh到ssh-agent</p><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>$ ssh-add c:/user/xxx/.ssh/id_ssh_gitlab
+$ ssh-add c:/user/xxx/.ssh/id_ssh_github
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div></li><li><p>添加config文件</p></li></ol><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>Host gitlab.com
+Hostname 192.168.1.178
+Port 8088
+IdentityFile ~/.ssh/id_ssh_github 
+
+Host github.com
+Hostname ssh.github.com
+Username xxx
+IdentityFile ~/.ssh/id_ssh_github
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><ol start="7"><li>测试连接</li></ol><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>ssh -T git@github.com
+ssh -T git@gitee.com
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div>`,5),d=[l];function t(r,c){return e(),i("div",null,d)}const h=s(n,[["render",t],["__file","ssh2.html.vue"]]);export{h as default};
