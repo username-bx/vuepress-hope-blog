b1b4f0bbe5ddab528263ab6cf5633fa5cbf1bf84
Benjamin Ma <benjamin_ma@manka.co.jp>
2024-06-13 11:32:02 +0900

    vue3

---
+++ assets/pageview-b4f6ff95.js
@@ -0,0 +1,1 @@
+import{l as t}from"./app-3524151c.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-3524151c.js").then(r=>r.Q),[]);return e({serverURL:"https://comments.luogesidoc.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
