22ad63a5b7616185eeaa1516e97c09d63274ad47
benjaminAltman <benjamin@altman.com>
2024-09-03 16:42:36 +0900

    git blog

--- assets/pageview-a2fc48d3.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"./app-7acabcd2.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-7acabcd2.js").then(r=>r.Q),[]);return e({serverURL:"https://comments.luogesidoc.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
