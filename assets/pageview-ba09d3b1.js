201deb196fc46871cad22daf0a9335ff65b42fac
benjaminAltman <benjamin@altman.com>
2024-09-04 16:37:58 +0900

    install jdk use brew, install nvm use brew, in mac

---
+++ assets/pageview-ba09d3b1.js
@@ -0,0 +1,1 @@
+import{l as t}from"./app-b6988e45.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-b6988e45.js").then(r=>r.Q),[]);return e({serverURL:"https://comments.luogesidoc.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
