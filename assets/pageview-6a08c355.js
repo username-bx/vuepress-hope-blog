10d819de1b9109b1861a8aa277055e833e142f7b
benjaminAltman <benjamin@altman.com>
2024-09-18 13:08:37 +0900

    oxygen not included

--- assets/pageview-6a08c355.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"./app-9a115dbf.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-9a115dbf.js").then(r=>r.Q),[]);return e({serverURL:"https://comments.luogesidoc.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
