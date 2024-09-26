216926f6a43e16d4660c0d9bd9347093e2079a5f
benjaminAltman <benjamin@altman.com>
2024-09-26 17:31:20 +0900

    table scroll

--- assets/pageview-fdd39912.js
+++
@@ -1,1 +0,0 @@
-import{l as t}from"./app-1f894b6c.js";const o=async()=>{try{const{pageviewCount:e}=await t(()=>import("./app-1f894b6c.js").then(r=>r.Q),[]);return e({serverURL:"https://comments.luogesidoc.com/"})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
